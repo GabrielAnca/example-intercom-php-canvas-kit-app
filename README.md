d0444fbed012e549c9cfc52a6ad5ed62509c56a2
Gabriel Anca <gabrielancacorral@gmail.com>
2020-05-20 15:27:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Simple Intercom Canvas Kit app built in PHP
+
+This example was tested using PHP 7.4
+
+See the complete API reference in the [documentation](https://developers.intercom.com/canvas-kit-reference/reference/introduction).
+
+## Set up
+
+Just boot the application:
+
+```
+symfony server:start --port:8001
+```
+
+To point Intercom traffic to this environment, you can use ngrok:
+
+```
+ngrok http 8001
+```
+
+Create an app in Intercom's [Developer Hub](https://app.intercom.com/a/apps/_/developer-hub), with a Canvas Kit app pointing to this environment:
+
+* For users, leads and visitors:
+  * Initialize flow webhook URL: `https://<my-ngrok-id>.ngrok.io/messenger_app/initialize`
+  * Submit flow webhook URL: `https://<my-ngrok-id>.ngrok.io/messenger_app/submit`
+
+Implementation for teammates would be very similar.
---
+++ src/Controller/MessengerAppController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MessengerAppController extends AbstractController
+{
+    /**
+     * @Route("/messenger_app/initialize", methods={"POST"}, name="messenger_app_initialize")
+     */
+    public function initialize()
+    {
+        return $this->json([
+            'canvas' => [
+                'content' => [
+                    'components' => [
+                        [
+                            "type" => "text",
+                            "id" => "text-1",
+                            "text" => "Hello world! (from initialize action)",
+                            "style" => "header"
+                        ],
+                        [
+                            "type" => "button",
+                            "id" => "submit-initialize",
+                            "label" => "Submit this canvas!",
+                            "action" => [
+                              "type" => "submit"
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * @Route("/messenger_app/submit", methods={"POST"}, name="messenger_app_submit")
+     */
+    public function submit(Request $request)
+    {
+        $requestBody = json_decode($request->getContent());
+        $componentId = $requestBody->component_id;
+
+        return $this->json([
+            'canvas' => [
+                'content' => [
+                    'components' => [
+                        [
+                            "type" => "text",
+                            "id" => "text-1",
+                            "text" => "Hello world! (from submit action)",
+                            "style" => "header"
+                        ],
+                        [
+                            "type" => "text",
+                            "id" => "text-2",
+                            "text" => sprintf("The '%s' component was clicked", $componentId),
+                        ],
+                        [
+                            "type" => "button",
+                            "id" => "submit-submit",
+                            "label" => "Submit this canvas again!",
+                            "action" => [
+                              "type" => "submit"
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+    }
+}
